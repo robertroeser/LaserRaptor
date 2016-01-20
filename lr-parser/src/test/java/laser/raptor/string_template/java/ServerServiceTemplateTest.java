52c32ffcf92d55cea43c1c7bed1147c805143882
Robert Roeser <rroeser@netflix.com>
2016-01-20 13:08:34 -0800

    fixed unit test

--- lr-parser/src/test/java/laser/raptor/string_template/java/ServerServiceTemplateTest.java
+++ lr-parser/src/test/java/laser/raptor/string_template/java/ServerServiceTemplateTest.java
@@ -109,9 +109,9 @@ public class ServerServiceTemplateTest {
 
         List<ServerServiceTemplate.RenderedService> render = serverServiceTemplate.render();
         render.forEach(s -> {
-            Assert.assertNotNull(s);
-            System.out.println(s);
-            Assert.assertTrue(s.getSource().contains("public class FooService_foo implements RequestResponseServerFunction<String, String> {"));
+            Assert.assertNotNull("rendered content is null", s);
+            System.out.println(s.getSource());
+            Assert.assertTrue("not rendering expected content", s.getSource().contains("public abstract class BaseFooService_foo implements RequestResponseServerFunction<String, String> {"));
         });
     }
 
