b67a891a3ea5087739ab31b99fd31b9bf52817cd
Robert Roeser <rroeser@netflix.com>
2016-01-19 17:51:22 -0800

    added @Inherited meta annotation and prefixed all the ServerFunctions with Base

--- lr-java-server/src/main/java/laser/raptor/core/server/LaserRaptorServerFunctionMetadata.java
+++ lr-java-server/src/main/java/laser/raptor/core/server/LaserRaptorServerFunctionMetadata.java
@@ -3,12 +3,14 @@ package laser.raptor.core.server;
 import laser.raptor.core.server.server_functions.ServerFunction;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface LaserRaptorServerFunctionMetadata {
     long hash();
     int serviceId();
